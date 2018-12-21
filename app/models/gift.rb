6808e0b74044c425ddda3d5941a1777d46ff2498
nagaminenot <nagamine@slogan.jp>
2018-12-22 05:07:01 +0900

    giftモデルのバリデーション・保存前処理作成

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,2 +1,15 @@
 class Gift < ApplicationRecord
+  require "securerandom"
+  before_save :prepare_save
+  validates :giver_name, presence: true
+  validates :taker_name, presence: true
+  validates :slug, uniqueness: true
+
+  def prepare_save
+    if self.slug.present?
+      true
+    else
+      self.slug = SecureRandom.hex(32)
+    end
+  end
 end
