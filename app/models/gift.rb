c40bbd9310b0b6024c09fb245a1e87a6660ff1f3
nagaminenot <nagamine@slogan.jp>
2018-12-22 05:25:30 +0900

    giftモデルのメソッド追加

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,15 +1,21 @@
 class Gift < ApplicationRecord
   require "securerandom"
-  before_save :prepare_save
+  before_save :set_slug
+  after_save :update_ogp
   validates :giver_name, presence: true
   validates :taker_name, presence: true
   validates :slug, uniqueness: true
 
-  def prepare_save
+  def set_slug
     if self.slug.present?
       true
     else
       self.slug = SecureRandom.hex(32)
     end
+  end
+
+  def update_ogp
+    ogp_image_generator = GiftOgpImageGenerator.new(self)
+    ogp_image_generator.generate
   end
 end
