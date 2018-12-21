dfd9aa1fbc499ebd07f9b9d681f3059b54d03bd1
nagaminenot <nagamine@slogan.jp>
2018-12-22 02:27:51 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_12_21_171439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gifts", force: :cascade do |t|
+    t.string "gift_name"
+    t.string "giver_name"
+    t.string "taker_name"
+    t.string "gift_url"
+    t.string "image_url"
+    t.string "message"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
