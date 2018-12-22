9fa7ab06494113484ea3f06ac97d77d0653f1362
nagaminenot <nagamine@slogan.jp>
2018-12-22 12:27:22 +0900

    除外の方法間違えていた

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root  'gifts#new'
-  resources :gifts, except => :index
+  resource :gifts, except: [:index]
   get '/gifts/complete/:id', to: 'gifts#complete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
