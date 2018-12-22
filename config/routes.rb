dbe374ca67baf93fdf325ec25a4b303695c438bf
nagaminenot <nagamine@slogan.jp>
2018-12-22 12:25:33 +0900

    complete画面作成、index除外など

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root  'gifts#new'
-  resources :gifts
+  resources :gifts, except => :index
+  get '/gifts/complete/:id', to: 'gifts#complete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
